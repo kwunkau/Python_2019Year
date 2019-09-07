9d0cd781a6dac8a5a789446a0467cab7e5dd7aaf
Administrator <289291077@qq.com>
2019-09-07 16:27:41 +0800

    20190907

--- 剑指Offer/reference/面试题3 二维数组中的查找.py
+++ 剑指Offer/reference/面试题3 二维数组中的查找.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
+
+'''
+@Author:    kabuto
+@Date:      2019/06/26
+'''
+
 '''
 【题目】：
 在一个二维数组中，每一行都按照从左到右递增的顺序排列，每一列按照从上到下的顺序排列。完成一个函数，使得输入一个二维数组和一个整数，判断数组中是否有该整数。
+
 【解题思路】：
 每次从左下角或者右上角的数字开始进行比较（书中是从右上角开始）。这里我们选取左下角的数开始比较：
 1. 如果左下角的数等于查找数， 则返回True；
@@ -9,29 +16,29 @@
 3. 如果左下角的数小于查找数， 则比较同一行中的右边一位的数字；
 '''
 
-def Find(A, number):
-    if not A:
+def Find(grid, number) -> bool:
+    if not grid:
         return False
-    row, col = len(A), len(A[0])
+    row, col = len(grid), len(grid[0])
     cur_i, cur_j = row - 1, 0
 
     while cur_i >= 0 and cur_j < col:
-        if A[cur_i][cur_j] == number:
+        if grid[cur_i][cur_j] == number:
             return True
-        elif A[cur_i][cur_j] < number:
+        elif grid[cur_i][cur_j] < number:
             cur_j += 1
         else:
             cur_i -= 1
 
     return False
 
-A = [[1, 2, 8, 9],
+grid = [[1, 2, 8, 9],
        [2, 4, 9, 12],
        [4, 7, 10, 13],
        [6, 8, 11, 15]]
 
-A1 = []
+grid1 = []
 
 number = 9
 
-print(Find(A1, number))
+Find(grid1, number)
