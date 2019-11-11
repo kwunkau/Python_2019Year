6ed1d0dfa672541969197249e3cc28f627cf7f10
kwunkau <289291077@qq.com>
2019-11-12 00:12:58 +0800

    20191111

---
+++ 1021/test.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#__author__ = "Ligq"
+
+import pandas as pd
+#显示所有列
+pd.set_option('display.max_columns', None)
+#显示所有行
+pd.set_option('display.max_rows', None)
